2dc4029b18988baa96a2ca0e4cc6bde9816d8da0
lijiongjiong <lijiongjiong163@163.com>
2020-06-06 14:06:59 +0800

    配置文件

--- 01_helloSB/src/main/java/com/ljj/hello/domain/Person.java
+++ 01_helloSB/src/main/java/com/ljj/hello/domain/Person.java
@@ -1,12 +1,14 @@
 package com.ljj.hello.domain;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Map;
 
 /**
- * @ConfigurationProperties(prefix = "person")
+ * @ConfigurationProperties(prefix = "person")  从全局配置文件中取值
  * 这个类必须要加载到spring容器中
  * prefix中必须全小写字母
  * 这个类中的属性必须有set方法
@@ -14,8 +16,11 @@ import java.util.Map;
  */
 @Component
 @ConfigurationProperties(prefix = "person")
+@Validated
 public class Person {
+    //@Value("${person.name}")
     private String name;
+
     private int age;
     private Map myMap;
     private String array;
