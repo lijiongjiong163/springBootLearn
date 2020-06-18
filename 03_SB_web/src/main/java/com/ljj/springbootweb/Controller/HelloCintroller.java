f0b4b511c38e9783bfce6df26b4c9a9a0fa4070c
lijiongjiong <lijiongjiong163@163.com>
2020-06-18 19:22:13 +0800

    thymeleafDemo

--- 03_SB_web/src/main/java/com/ljj/springbootweb/Controller/HelloCintroller.java
+++ 03_SB_web/src/main/java/com/ljj/springbootweb/Controller/HelloCintroller.java
@@ -3,13 +3,15 @@ package com.ljj.springbootweb.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.Map;
 
 @Controller
 public class HelloCintroller {
     @RequestMapping("success")
     public String success(Map<String,Object> map){
-        map.put("name","小明");
+        map.put("name","<h1>小明</h1>");
+        map.put("users", Arrays.asList("张三","李四","王五"));
         return "success";
     }
 }
